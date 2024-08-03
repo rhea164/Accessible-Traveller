213d400343599e65b58a5932d253c04e47cfc0ef
Bratee Podder <bpodder3@gatech.edu>
2024-08-03 14:21:12 +0400

    making landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen justify-center items-center flex-col">
+      <div className="flex flex-col items-center justify-center p-14 gap-y-10">
+        <h1 className="text-6xl">Hi, where would you like to go?</h1>
+        <h4 className="text-2xl">Tell us your preferences</h4>
+      </div>
+      <div className="flex border-4 w-2/3 p-3 rounded-lg">
+        <div className="flex flex-row">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Search 
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
