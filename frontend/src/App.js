a05673b8a73c78a9307715a2c23b38b4cb346243
Bratee Podder <bpodder3@gatech.edu>
2024-08-03 15:36:19 +0400

    Frontend Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Maps from './pages/Map';
 
-function App() {
+const App = () => {
   return (
-    <div className="flex h-screen justify-center items-center flex-col">
-      <div className="flex flex-col items-center justify-center p-14 gap-y-10">
-        <h1 className="text-6xl">Hi, where would you like to go?</h1>
-        <h4 className="text-2xl">Tell us your preferences</h4>
-      </div>
-      <div className="flex border-4 w-2/3 p-3 rounded-lg">
-        <div className="flex flex-row">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Search 
-          </button>
-        </div>
-      </div>
+    <div>
+      <Header />
+      <main className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/maps" element={<Maps />} />
+        </Routes>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
