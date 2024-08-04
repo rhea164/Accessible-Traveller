12a9da3922676626ecd524149970cdc5083978df
Bratee Podder <bpodder3@gatech.edu>
2024-08-04 12:40:45 +0400

    committing some changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,11 +7,10 @@ const Header = () => {
   return (
     <div className='flex justify-center mx-32 my-12 items-center'>
       <header className="flex flex-row w-full border-4 border-off-white bg-off-white text-black py-3 rounded-lg shadow-lg items-center">
+        <div className="flex justify-center w-full">
+          <img src={logo} alt="logo" className="mx-5 h-10 align-left" />
 
-        <img src={logo} alt="logo" className="mx-5 h-10" />
-
-        <div className="flex justify-end w-full">
-          <nav className="flex justify-end">
+          <nav className="flex justify-center ">
             <Link to="/" className="hover:bg-lime-950 hover:text-white mx-6 py-2 px-4 rounded-lg transition duration-300 ease-in-out">
               Home
             </Link>
